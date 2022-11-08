02f644795e3f4a56c4303ba7773c82ca7e84cfd9
Vitor Longarini <vlongarini@outlook.com>
2022-11-07 21:25:34 -0300

    Teste Cloud 2

--- config/express.js
+++ config/express.js
@@ -6,7 +6,7 @@ require("dotenv-safe").config();
 module.exports = () => {
     const app = express();
 
-    app.set('port', (process.env.PORT || 8080));
+    app.set('port', (process.env.PORT || 3000));
 
     app.use(bodyParser.json());
 
